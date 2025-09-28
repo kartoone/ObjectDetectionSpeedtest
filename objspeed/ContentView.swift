2cbba13784c59716d9d7062c908d4a89ef9ff33d
kartoone <brtoone@samford.edu>
2025-09-28 07:27:01 -0500

    Initial Commit

---
+++ objspeed/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  objspeed
+//
+//  Created by Brian Toone on 9/28/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ objspeed/objspeedApp.swift
@@ -0,0 +1,17 @@
+//
+//  objspeedApp.swift
+//  objspeed
+//
+//  Created by Brian Toone on 9/28/25.
+//
+
+import SwiftUI
+
+@main
+struct objspeedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
